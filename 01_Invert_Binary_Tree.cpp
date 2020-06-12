df0a133968bb3710203c7fde4a3eec97e8eb5e86
Mayank Tiwari <tmayanc@gmail.com>
2020-06-12 10:52:50 +0530

    Update 01_Invert_Binary_Tree.cpp

--- 01_Invert_Binary_Tree.cpp
+++ 01_Invert_Binary_Tree.cpp
@@ -1,3 +1,26 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+*/
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
