7d7ef162dfe9277159e2a8eb095b33c62e02a35f
Mayank Tiwari <tmayanc@gmail.com>
2020-06-22 11:47:33 +0530

    Create 15_Search_in_Binary_Search_Tree.cpp

---
+++ 15_Search_in_Binary_Search_Tree.cpp
@@ -0,0 +1,43 @@
+/* Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int key) {
+        
+        if(root == NULL)
+            return NULL;
+        
+        if(root -> val == key)
+            return root;
+        
+        else if(root -> val > key)
+            return searchBST(root -> left, key);
+        
+        else
+            return searchBST(root -> right, key);
+        
+    }
+};
